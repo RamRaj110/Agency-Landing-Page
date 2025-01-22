783a8af01972b176abf26cc0e40e003e6195f458
Mr. Pawan <89289684+RamRaj110@users.noreply.github.com>
2025-01-22 18:37:18 +0530

    Image view change

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -36,7 +36,7 @@ function Project() {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                 },
             },
